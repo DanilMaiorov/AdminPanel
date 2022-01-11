e16dabb0677446052056c50d2454b66d4de5e578
DanilMaiorov <damaiorov93@gmail.com>
2022-01-11 17:39:38 +0500

    LESSON29

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import module_1 from './modules/module_1';
+import { render } from './modules/render'; //импортируем через {} потому что экспортировали не по дефолту
+import { UserService } from './modules/userService'; //импортируем через {} потому что экспортировали не по дефолту
 
-module_1();
+//после импорта UserService создадим новый экземпляр класса
+//const userService = new UserService // и его теперь будем передавать в любой модуль и исплльзовать //НО ЭТОТ МЕТОД НЕ САМЫЙ ЛУЧШИЙ, МОЖЕМ ЕГО УДАЛИТЬ И СДЕЛАТЬ (В КОДЕ НИЖЕ)
+
+window.userService = new UserService // в таком случае нам не обязательно передавать userService в переменныее, потому что это стал глобальный объект
+
+
+render(userService.users);//передадим userService.users
